4b23797b9846e4a7a9df98f2e0d023e99d5ed106
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-10 12:29:07 +0200

    edit form component

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -1,52 +1,109 @@
-export default function Form({ recipe, isEdit }) {
+import styled from "styled-components";
+
+export default function Form({ recipe, isEdit, onSubmit }) {
   const { author, description, title, image, neededItems } = recipe;
   const neededItemsString = neededItems.join(", ");
 
   return (
-    <form>
-      <label htmlFor="title">Title:</label>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        defaultValue={isEdit && title}
-      />
-      <label htmlFor="description">Recipe:</label>
-      <textarea
-        type="text"
-        name="description"
-        id="description"
-        defaultValue={isEdit && description}
-      />
-      <label htmlFor="neededItems">Needed Items: (seperate by Comma)</label>
-      <input
-        type="text"
-        name="neededItems"
-        id="neededItems"
-        defaultValue={isEdit && neededItemsString}
-      />
-      <label htmlFor="image">Recipe Image:</label>
-      <input
-        type="text"
-        name="image"
-        id="image"
-        defaultValue={isEdit && image}
-      />
-      <label htmlFor="author-name">Author Name:</label>
-      <input
-        type="text"
-        name="author-name"
-        id="author-name"
-        defaultValue={isEdit && author.name}
-      />
-      <label htmlFor="author-avatar">Author Avatar:</label>
-      <input
-        type="text"
-        name="author-avatar"
-        id="author-avatar"
-        defaultValue={isEdit && author.avatarSource}
-      />
-      <button type="submit">Save</button>
-    </form>
+    <>
+      <StyledForm onSubmit={onSubmit}>
+        <FormWrapper>
+          <StyledLabel htmlFor="title">Title:</StyledLabel>
+          <StyledInput
+            type="text"
+            name="title"
+            id="title"
+            defaultValue={isEdit && title}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <StyledLabel htmlFor="description">Recipe:</StyledLabel>
+          <StyledTextarea
+            type="text"
+            name="description"
+            id="description"
+            defaultValue={isEdit && description}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <StyledLabel htmlFor="neededItems">
+            Ingredients: (seperate by Comma)
+          </StyledLabel>
+          <StyledInput
+            type="text"
+            name="neededItems"
+            id="neededItems"
+            defaultValue={isEdit && neededItemsString}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <StyledLabel htmlFor="image">Recipe Image:</StyledLabel>
+          <StyledInput
+            type="text"
+            name="image"
+            id="image"
+            defaultValue={isEdit && image}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <StyledLabel htmlFor="author-name">Author Name:</StyledLabel>
+          <StyledInput
+            type="text"
+            name="author-name"
+            id="author-name"
+            defaultValue={isEdit && author.name}
+          />
+        </FormWrapper>
+        <FormWrapper>
+          <StyledLabel htmlFor="author-avatar">Author Avatar:</StyledLabel>
+          <StyledInput
+            type="text"
+            name="author-avatar"
+            id="author-avatar"
+            defaultValue={isEdit && author.avatarSource}
+          />
+        </FormWrapper>
+      </StyledForm>
+      <SubmitButton type="submit">Save</SubmitButton>
+    </>
   );
 }
+
+const StyledForm = styled.form`
+  display: table;
+`;
+
+const StyledInput = styled.input`
+  display: table-cell;
+  width: 100%;
+  height: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const StyledLabel = styled.label`
+  display: table-cell;
+  width: 40%;
+  margin-bottom: 1rem;
+  font-weight: bold;
+`;
+
+const StyledTextarea = styled.textarea`
+  display: table-cell;
+  width: 100%;
+  margin-bottom: 1rem;
+`;
+
+const FormWrapper = styled.div`
+  display: table-row;
+`;
+
+const SubmitButton = styled.button`
+  padding: 1rem;
+  font-weight: bold;
+  border-radius: 10px;
+  border: none;
+  color: #1c1c1c;
+  background: #00cc99;
+  outline: 1px solid #1c1c1c;
+  font-size: 1.1rem;
+`;
--- pages/[id]/edit.js
+++ pages/[id]/edit.js
@@ -6,10 +6,15 @@ export default function EditPage({ recipes }) {
   const id = useRouter().query.id;
   const recipe = recipes.find((recipe) => recipe._id === id);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = Object.fromEntries(new FormData(event.target));
+  };
+
   return (
     <BigCard>
       <h2>Edit Recipe</h2>
-      <Form recipe={recipe} isEdit={true} />
+      <Form recipe={recipe} isEdit={true} onSubmit={handleSubmit} />
     </BigCard>
   );
 }
