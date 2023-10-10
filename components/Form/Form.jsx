2dd89aaa78dda74ce573d82a450c7bcb679dc880
Wilhelm Werwein <werwein.dev@gmail.com>
2023-10-10 12:08:04 +0200

    edit Form Component

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -1,26 +1,51 @@
 export default function Form({ recipe, isEdit }) {
-  console.log(recipe);
   const { author, description, title, image, neededItems } = recipe;
+  const neededItemsString = neededItems.join(", ");
 
   return (
     <form>
       <label htmlFor="title">Title:</label>
-      <input type="text" name="title" id="title" value={isEdit && title} />
+      <input
+        type="text"
+        name="title"
+        id="title"
+        defaultValue={isEdit && title}
+      />
       <label htmlFor="description">Recipe:</label>
       <textarea
         type="text"
         name="description"
         id="description"
-        value={isEdit && description}
+        defaultValue={isEdit && description}
       />
       <label htmlFor="neededItems">Needed Items: (seperate by Comma)</label>
-      <input type="text" name="neededItems" id="neededItems" />
+      <input
+        type="text"
+        name="neededItems"
+        id="neededItems"
+        defaultValue={isEdit && neededItemsString}
+      />
       <label htmlFor="image">Recipe Image:</label>
-      <input type="text" name="image" id="image" />
+      <input
+        type="text"
+        name="image"
+        id="image"
+        defaultValue={isEdit && image}
+      />
       <label htmlFor="author-name">Author Name:</label>
-      <input type="text" name="author-name" id="author-name" />
+      <input
+        type="text"
+        name="author-name"
+        id="author-name"
+        defaultValue={isEdit && author.name}
+      />
       <label htmlFor="author-avatar">Author Avatar:</label>
-      <input type="text" name="author-avatar" id="author-avatar" />
+      <input
+        type="text"
+        name="author-avatar"
+        id="author-avatar"
+        defaultValue={isEdit && author.avatarSource}
+      />
       <button type="submit">Save</button>
     </form>
   );
--- db/models/Recipe.js
+++ db/models/Recipe.js
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const recipeSchema = new Schema({
-  title: { type: String, required: true },
-  image: { type: String, required: true },
-  description: { type: String, required: true },
-  author: {
-    name: { type: String, required: true },
-    avatarSource: { type: String },
+const recipeSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    author: {
+      name: { type: String, required: true },
+      avatarSource: { type: String },
+    },
+    neededItems: { type: Array, required: true },
   },
-  neededItems: { type: Array, required: true },
-});
+  { timestamps: true }
+);
 
 const Recipe = mongoose.models.Recipe || mongoose.model("Recipe", recipeSchema);
 export default Recipe;
